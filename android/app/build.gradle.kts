48a62339b3ff963158bebe23c01fe58468a5daeb
Raj Kateshiya <rajkateshiya.rk12@gmail.com>
2026-05-11 12:04:18 +0530

    # new release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.steptechnovision.aipillidentifier"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
         multiDexEnabled = true
     }
 
